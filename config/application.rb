cb2d4503b7e3edf18ed4b5efa987c0722afa3be5
fakefarm <dave@woodalls.me>
2017-08-17 18:41:46 -0400

    do not generate assest or helpers

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,9 @@ module Scaffold
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
 
+    config.generators.assets = false
+    config.generators.helper = false
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
