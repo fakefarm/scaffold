f4720f443f3456cfa01f468b3c9766f4536ce09d
fakefarm <dave@woodalls.me>
2017-08-17 22:57:46 -0400

    Add association tests to User

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,9 @@
 class User < ApplicationRecord
   validates :first_name, :last_name, :email, presence: true
+  has_many :activities
+  has_many :details
+  has_many :products
+  has_many :projects
+  has_many :retros
+  has_many :tasks
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,25 +1,56 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-    setup do
-      @user = users(:invalid)
-    end
+  setup do
+    @user = users(:invalid)
+  end
 
-    test 'is invalid without a first_name' do
-      assert_equal @user.valid?, false
-    end
+  test 'is invalid without a first_name' do
+    assert_equal @user.valid?, false
+  end
 
-    test 'is invalid without a last_name' do
-      assert_equal @user.valid?, false
-    end
+  test 'is invalid without a last_name' do
+    assert_equal @user.valid?, false
+  end
 
-    test 'is invalid without an email' do
-      assert_equal @user.valid?, false
-    end
+  test 'is invalid without an email' do
+    assert_equal @user.valid?, false
+  end
 
-    test 'valid with all attributes' do
-      attributes = { first_name: 'Dave', last_name: 'Woodall', email: 'dave@woodalls.me'}
-      @user.update_attributes(attributes)
-      assert_equal @user.valid?, true
-    end
+  test 'valid with all attributes' do
+    attributes = { first_name: 'Dave', last_name: 'Woodall', email: 'dave@woodalls.me'}
+    @user.update_attributes(attributes)
+    assert_equal @user.valid?, true
+  end
+
+  test "has many activities" do
+    @user = users(:one)
+    assert @user.activities
+  end
+  
+  test "has many details" do
+    @user = users(:one)
+    assert @user.details
+  end
+
+  test "has many products" do
+    @user = users(:one)
+    assert @user.products
+  end
+
+  test "has many projects" do
+    @user = users(:one)
+    assert @user.projects
+  end
+
+  test "has many retros" do
+    @user = users(:one)
+    assert @user.retros
+  end
+
+  test "has many tasks" do
+    @user = users(:one)
+    assert @user.tasks
+  end
+
 end
