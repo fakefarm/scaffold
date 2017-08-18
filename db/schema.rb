206becebd42ac33b948baed6005b82e6da5dfc1f
fakefarm <dave@woodalls.me>
2017-08-17 20:41:25 -0400

    add activities

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170817232800) do
+ActiveRecord::Schema.define(version: 20170818003810) do
+
+  create_table "activities", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "product_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["product_id"], name: "index_activities_on_product_id"
+    t.index ["user_id"], name: "index_activities_on_user_id"
+  end
 
   create_table "products", force: :cascade do |t|
     t.string "name"
